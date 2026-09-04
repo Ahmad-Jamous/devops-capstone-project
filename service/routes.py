bf26cc3fffefd6a4e1bf8f464151d03df2c5960b
Ahmad-Jamous <ahmad.jamous@outlook.de>
2026-09-04 08:26:56 -0400

    fixed the routes.py like flake8 wanted

--- service/routes.py
+++ service/routes.py
@@ -65,6 +65,7 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
+    
     """
     List all Accounts
     This endpoint will list all Accounts
@@ -85,15 +86,18 @@ def list_accounts():
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
+    
     """
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
     """
+
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id  [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -105,10 +109,12 @@ def read_account(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
+    
     """
     Update an Account
     This endpoint will update an Account based on the posted data
     """
+
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -127,10 +133,12 @@ def update_accounts(account_id):
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
+
     """
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
     """
+    
     app.logger.info("Request to delete an Account with id: %s", account_id)
 
     account = Account.find(account_id)
